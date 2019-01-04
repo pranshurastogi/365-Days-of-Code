7b8deee57fc1402654a2b344d3312d1e9d5d0f00
Pranshu_Ubuntu <pranshurastogi3196@gmail.com>
2019-01-04 12:00:53 +0530

    Loops in Python

---
+++ 4.Jan.2019/Solution.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(10):
+        tot = n*(i+1)
+        print(n ,"x" , i+1 , "=" , tot)
+       
+
+
+# In[ ]:
+
+
+
+
